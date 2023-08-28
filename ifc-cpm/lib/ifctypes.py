3c296369cbeffa4d062b158dae1930f1e36859ba
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-08-28 14:00:29 +1000

    Fix classes

--- ifc-cpm/lib/ifctypes.py
+++ ifc-cpm/lib/ifctypes.py
@@ -8,16 +8,19 @@ class BuildingElement:
     start_vertex: Tuple[float, float] = None
     end_vertex: Tuple[float, float] = None
 
-    def __init__(self, name=None, start_vertex=None, end_vertex=None):
+    def __init__(
+        self, name=None, start_vertex=None, end_vertex=None, type="BuildingElement"
+    ):
         self.name = name
         self.start_vertex = start_vertex
         self.end_vertex = end_vertex
+        self.__type__ = type
 
     @property
     def length(self):
         (x1, y1), (x2, y2) = self.start_vertex, self.end_vertex
         return np.sqrt((abs(x2 - x1) ** 2) + (abs(y2 - y1) ** 2))
-    
+
     def __repr__(self):
         return f"{self.__type__}({self.name}, {self.start_vertex}, {self.end_vertex})"
 
@@ -25,6 +28,12 @@ class BuildingElement:
 class Wall(BuildingElement):
     __type__ = "Wall"
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs, type="Wall")
+
 
 class Gate(BuildingElement):
     __type__ = "Gate"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs, type="Gate")
