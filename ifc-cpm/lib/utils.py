f7c9c5628e0a26b8f17685a55ee08386625ae024
Muhammad Aditya Hilmy <mhilmy@hey.com>
2023-10-09 16:17:02 +1100

    Fix truncate bug

--- ifc-cpm/lib/utils.py
+++ ifc-cpm/lib/utils.py
@@ -28,11 +28,11 @@ def transform_vertex(transformation_matrix, vertex: Tuple[float, float]) -> Tupl
 # TODO find permanent solution for rounding error
 def truncate(number, digits=4) -> float:
     # Improve accuracy with floating point operations, to avoid truncate(16.4, 2) = 16.39 or truncate(-1.13, 2) = -1.12
-    nbDecimals = len(str(number).split('.')[1])
-    if nbDecimals <= digits:
+    components = str(number).split('.')
+    if len(components) == 1 or len(components[1]) <= digits:
         return number
     stepper = 10.0 ** digits
-    return math.trunc(stepper * number) / stepper
+    return math.ceil(stepper * number) / stepper
 
 
 def find_unbounded_lines_intersection(line1, line2):
