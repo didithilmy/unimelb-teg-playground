370df2a1829c648c469f9d544c0f699e594d8ce7
Muhammad Aditya Hilmy <mhilmy@hey.com>
2023-10-13 11:53:59 +1100

    Add floor voids to unparsable

--- ifc-cpm/lib/unparsable.py
+++ ifc-cpm/lib/unparsable.py
@@ -12,6 +12,16 @@ def get_unparsable_elements(ifc_building) -> List[Any]:
     unparsable_elements = []
     elements = ifcopenshell.util.element.get_decomposition(ifc_building)
     for element in elements:
-        if element.is_a("IfcTransportElement"):  # Escalators, elevators, travelators
+        # Escalators, elevators, travelators
+        if element.is_a("IfcTransportElement"):
             unparsable_elements.append((element, "Element is not yet supported"))
+
+        # Floor voids
+        if element.is_a("IfcSlab"):
+            if element.PredefinedType == "FLOOR":
+                floor_openings = element.HasOpenings
+                for floor_opening in floor_openings:
+                    opening_element = floor_opening.RelatedOpeningElement
+                    unparsable_elements.append((opening_element, "Floor slab void is not yet supported"))
+
     return unparsable_elements
