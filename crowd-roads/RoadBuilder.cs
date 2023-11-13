0becb79046f8b606db383ddfdf79fe34da4f3d50
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2023-11-13 15:18:57 +1100

    Create intersection

--- crowd-roads/RoadBuilder.cs
+++ crowd-roads/RoadBuilder.cs
@@ -1,11 +1,12 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using EasyRoads3Dv3;
-
+using Michsky.MUIP;
+using System; // MUIP namespace
 
 public class RoadBuilder : MonoBehaviour
 {
+    public CustomDropdown elementPickerDropdown;
     public Vector3 screenPosition, worldPosition;
     public Vector3 startCoord;
     public Vector3 endCoord;
@@ -15,56 +16,87 @@ public class RoadBuilder : MonoBehaviour
 
     private bool dragging = false;
     private ERRoad currentRoad;
+    private ERConnection currentCrossing;
     private Collider coll;
 
-    void Start() {
-        Debug.Log("Please read the comments at the top of the runtime script (/Assets/EasyRoads3D/Scripts/runtimeScript) before using the runtime API!");
-        
-        roadNetwork = new ERRoadNetwork();
-
-        roadType = new ERRoadType();
-		roadType.roadWidth = 6;
-		roadType.roadMaterial = Resources.Load("Materials/roads/road material") as Material;
-        roadType.layer = 1;
-        roadType.tag = "Untagged";
-
-        coll = GetComponent<Collider>();
+    enum ConnectionType
+    {
+        CrossingX,
+        CrossingT
     }
 
-    void Update() {
+    void Start()
+    {
+        Debug.Log("Please read the comments at the top of the runtime script (/Assets/EasyRoads3D/Scripts/runtimeScript) before using the runtime API!");
+
+        coll = GetComponent<Collider>();
+        roadNetwork = new ERRoadNetwork();
+        roadType = roadNetwork.GetRoadTypeByName("Default Road");
+        roadNetwork.LoadConnections();
+    }
+
+    void Update()
+    {
         Vector3? coord = GetMouseWorldCoord();
-        if (coord != null) {
-            endCoord = (Vector3) coord;
+        if (coord == null)
+        {
+            return;
         }
-        if (dragging && currentRoad != null) {
-            if (coord != null) {
-                UpdateRoadEndCoord(currentRoad, (Vector3) coord);
+
+        endCoord = SnapToGrid((Vector3)coord);
+        if (dragging)
+        {
+            if (currentRoad != null)
+            {
+                UpdateRoadEndCoord(currentRoad, endCoord);
+            }
+            else if (currentCrossing != null)
+            {
+                UpdateCrossingCoord(currentCrossing, endCoord);
             }
         }
     }
 
-    void OnMouseDown() {
+    void OnMouseDown()
+    {
         dragging = true;
         Vector3? startCoord = GetMouseWorldCoord();
-        if (startCoord != null) {
-            currentRoad = CreateRoad("Road", roadType, (Vector3) startCoord);
-            currentRoad.SetWidth(5);
+        if (startCoord != null)
+        {
+            Vector3 coord = SnapToGrid((Vector3)startCoord);
+            switch (elementPickerDropdown.selectedItemIndex)
+            {
+                case 1:
+                    currentRoad = CreateRoad("Road", roadType, coord, 5);
+                    break;
+                case 2:
+                    currentCrossing = CreateCrossing(ConnectionType.CrossingX, coord);
+                    break;
+                case 3:
+                    currentCrossing = CreateCrossing(ConnectionType.CrossingT, coord);
+                    break;
+            }
         }
-    }
-    
-    void OnMouseUp() {
-        dragging = false;
-        HashSet<ERRoad> connectedRoads = GetConnectedRoads(currentRoad);
-        foreach (ERRoad connectedRoad in connectedRoads) {
-            roadNetwork.ConnectRoads(currentRoad, connectedRoad);
-        }
-        // Debug.Log(connectedRoads.Count);
-        currentRoad = null;
     }
 
-    private Vector3? GetMouseWorldCoord() {
+    void OnMouseUp()
+    {
+        dragging = false;
+        // HashSet<ERRoad> connectedRoads = GetConnectedRoads(currentRoad);
+        // foreach (ERRoad connectedRoad in connectedRoads)
+        // {
+        //     roadNetwork.ConnectRoads(currentRoad, connectedRoad);
+        // }
+        // Debug.Log(connectedRoads.Count);
+        currentRoad = null;
+        currentCrossing = null;
+    }
+
+    private Vector3? GetMouseWorldCoord()
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (coll.Raycast(ray, out RaycastHit hit, 1000.0f)) {
+        if (coll.Raycast(ray, out RaycastHit hit, 1000.0f))
+        {
             Vector3 point = ray.GetPoint(hit.distance);
             point.y = 0.05f;
             return point;
@@ -73,35 +105,72 @@ public class RoadBuilder : MonoBehaviour
         return null;
     }
 
-    private Vector3 SnapToGrid(Vector3 pos) {
-        return new Vector3(Mathf.Round(pos.x),
+    private Vector3 SnapToGrid(Vector3 pos)
+    {
+        float factor = 3f;
+        return new Vector3(Mathf.Round(pos.x / factor) * factor,
                              pos.y,
-                             Mathf.Round(pos.z));
+                             Mathf.Round(pos.z / factor) * factor);
     }
 
-    private ERRoad CreateRoad(string name, ERRoadType roadType, Vector3 startCoord) {
+    private ERRoad CreateRoad(string name, ERRoadType roadType, Vector3 startCoord, int width = 5, int noOfLanes = 1)
+    {
         ERRoad road = roadNetwork.CreateRoad(name, roadType);
-        Vector3 coord = SnapToGrid(startCoord);
-        road.AddMarker(coord);
-        road.AddMarker(coord);
+        road.SetWidth(width);
+        road.AddMarker(startCoord);
+        road.AddMarker(startCoord);
+        ERSideWalk sideWalk = new ERSideWalk
+        {
+            sidewalkWidth = 3,
+            crosswalkdepth = 3
+        };
+        road.SetSidewalk(sideWalk, ERRoadSide.Both, true);
         return road;
     }
 
-    private void UpdateRoadEndCoord(ERRoad road, Vector3 endCoord) {
-        Vector3 coord = SnapToGrid(endCoord);
+    private void UpdateRoadEndCoord(ERRoad road, Vector3 coord)
+    {
         road.DeleteMarker(1);
         road.InsertMarker(coord);
     }
 
-    private HashSet<ERRoad> GetConnectedRoads(ERRoad currentRoad) {
+    private ERConnection CreateCrossing(ConnectionType connectionType, Vector3 coord)
+    {
+        String prefabName = "Default X Crossing";
+        switch (connectionType)
+        {
+            case ConnectionType.CrossingT:
+                prefabName = "Default T Crossing";
+                break;
+            case ConnectionType.CrossingX:
+                prefabName = "Default X Crossing";
+                break;
+        }
+
+        ERConnection connectionPrefab = roadNetwork.GetConnectionPrefabByName(prefabName);
+        ERConnection connection = roadNetwork.InstantiateConnection(connectionPrefab, "Intersection", coord, Vector3.zero);
+        return connection;
+    }
+
+    private void UpdateCrossingCoord(ERConnection connection, Vector3 coord)
+    {
+        connection.gameObject.transform.position = coord;
+    }
+
+
+    private HashSet<ERRoad> GetConnectedRoads(ERRoad currentRoad)
+    {
         Vector3 startCoord = currentRoad.GetMarkerPosition(0);
         Vector3 endCoord = currentRoad.GetMarkerPosition(1);
         HashSet<ERRoad> roads = new HashSet<ERRoad>();
-        foreach (ERRoad road in roadNetwork.GetRoadObjects()) {
+        foreach (ERRoad road in roadNetwork.GetRoadObjects())
+        {
             if (road == currentRoad) continue;
             Vector3[] markerPositions = road.GetMarkerPositions();
-            foreach (Vector3 pos in markerPositions) {
-                if (pos == startCoord || pos == endCoord) {
+            foreach (Vector3 pos in markerPositions)
+            {
+                if (pos == startCoord || pos == endCoord)
+                {
                     roads.Add(road);
                     break;
                 }
